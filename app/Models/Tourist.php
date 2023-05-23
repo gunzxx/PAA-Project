2da6f5dc7d83f7d3cbb32e01d20704c1bc018722
gunzxx <ggm5850@gmail.com>
2023-05-23 20:49:25 +0700

    update tourist model

--- app/Models/Tourist.php
+++ app/Models/Tourist.php
@@ -3,16 +3,25 @@
 namespace App\Models;
 
 use App\Models\Category;
+use Spatie\MediaLibrary\HasMedia;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\InteractsWithMedia;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Tourist extends Model
+class Tourist extends Model implements HasMedia
 {
-    use HasFactory;
+    use HasFactory, InteractsWithMedia;
 
     protected $guarded = ['id'];
 
     public function category(){
         return $this->belongsTo(Category::class);
+    }
+
+    public function registerMediaCollections(): void
+    {
+        $this
+            ->addMediaCollection('image')
+            ->singleFile();
     }
 }
