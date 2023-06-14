37658a2e3f348662deb688d4b780c09aa7de9510
gunzxx <ggm5850@gmail.com>
2023-06-14 16:24:43 +0700

    update pagination

--- app/Http/Controllers/fe/TouristController.php
+++ app/Http/Controllers/fe/TouristController.php
@@ -11,7 +11,7 @@ class TouristController extends Controller
 {
     public function index()
     {
-        $tourists = Tourist::paginate(10);
+        $tourists = Tourist::simplePaginate(10);
         return view('home.tourist.index',[
             'tourists'=>$tourists,
             'active'=>'tourist',
