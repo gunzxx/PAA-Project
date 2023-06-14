be596eec7484fd4e306521f243fff8c22df8948e
gunzxx <ggm5850@gmail.com>
2023-06-14 16:14:00 +0700

    update view

--- resources/views/home/tourist/index.blade.php
+++ resources/views/home/tourist/index.blade.php
@@ -52,9 +52,9 @@
                     </tbody>
                 </table>
             </div>
-            <div class="container-pagination-me">
-                {{ $tourists->links() }}
-            </div>
+        </div>
+        <div class="container-pagination-me">
+            {{ $tourists->links() }}
         </div>
     </main>
 
